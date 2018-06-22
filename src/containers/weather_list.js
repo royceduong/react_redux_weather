88571b0dbcf36e8c5c3b9e79f1c1b22188b46ebd
Royce Duong <royceduong@Royces-MacBook-Pro.local>
2018-06-22 12:44:40 -0700

    added google api

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
     renderWeather(cityData) {
@@ -9,11 +10,14 @@ class WeatherList extends Component {
         const temps = cityData.list.map(weather => weather.main.temp);
         const pressures = cityData.list.map(weather => weather.main.pressure);
         const humidities = cityData.list.map(weather => weather.main.humidity);
+        const { lat, lon } = cityData.city.coord;
 
+        console.log(lat, lon);
         console.log(pressures)
         return (
             <tr key={city}>
-                <td>{city}</td>
+                {/* <td>{city}</td> */}
+                <td><GoogleMap lon={lon} lat={lat}/></td>
                 <td><Chart data={temps} color="orange" units="K"/></td>
                 <td><Chart data={pressures} color="green" units="hPa"/></td>
                 <td><Chart data={humidities} color="blue" units="%"/></td>
