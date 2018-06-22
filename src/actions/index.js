6257ceea503de18e48d31ce62e2906a645193000
Royce Duong <royceduong@Royces-MBP.attlocal.net>
2018-06-22 13:01:42 -0700

    updated redux weather action to a const

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,7 +7,7 @@ export function fetchWeather(city){
     const request = axios.get(url);
     console.log('Request:', request);
     return {
-        type: 'FETCH_WEATHER',
+        type: FETCH_WEATHER,
         payload: request
     };
 }
